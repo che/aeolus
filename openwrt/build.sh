6c2e2393798c90274792a6f92a030922b1b69e43
che <che.pasha@gmail.com>
2016-07-23 03:59:51 +0300

    Build aeolus-sense packages has been added for OpenWRT

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -1,11 +1,14 @@
 
 AEOLUS_CURRENT_DIR="$(cd $(dirname "$0") && pwd -P)/"
+AEOLUS_PACKGES_DIR="${AEOLUS_CURRENT_DIR}packages/"
+AEOLUS_PACKGES_CONFIG_FILE="${AEOLUS_PACKGES_DIR}.config"
 AEOLUS_BUILD_DIR="${AEOLUS_CURRENT_DIR}build/"
 
 OPENWRT_GIT_PROJECT="openwrt"
-OPENWRT_GIT_BRANCH="chaos_calmer"
+OPENWRT_GIT_BRANCH="master"  # chaos_calmer
 OPENWRT_GIT_URL="https://github.com/openwrt/${OPENWRT_GIT_PROJECT}/archive/${OPENWRT_GIT_BRANCH}.tar.gz"
 OPENWRT_BUILD_DIR="${AEOLUS_BUILD_DIR}${OPENWRT_GIT_PROJECT}-${OPENWRT_GIT_BRANCH}/"
+OPENWRT_BUILD_PACKAGES_DIR="${OPENWRT_BUILD_DIR}feeds/packages/lang/"
 
 
 # Clean
@@ -28,22 +31,29 @@ curl -SL "$OPENWRT_GIT_URL" | tar -xz
 cd "$OPENWRT_BUILD_DIR"
 
 
-# Build ENV
-#git checkout chaos_calmer
+# Prepare packages
 ./scripts/feeds update -a
+
+for pkg_dir in $(ls "$AEOLUS_PACKGES_DIR")
+do
+    if [ -e "$OPENWRT_BUILD_PACKAGES_DIR" ]
+    then
+        if [ ! -e "${OPENWRT_BUILD_PACKAGES_DIR}${pkg_dir}" ]
+        then
+            cp -r "${AEOLUS_PACKGES_DIR}${pkg_dir}" "$OPENWRT_BUILD_PACKAGES_DIR"
+        fi
+    fi
+done
+
+
+# Build
+if [ -e "${OPENWRT_BUILD_DIR}.config" ]
+then
+    mv -f "${OPENWRT_BUILD_DIR}.config" "${OPENWRT_BUILD_DIR}.config.old"
+    cp "$AEOLUS_PACKGES_CONFIG_FILE" "$OPENWRT_BUILD_DIR"
+fi
 ./scripts/feeds install -a
-#make kernel_defconfig
-#make defconfig
 make tools/install
 make toolchain/install
-make menuconfig
 make package/index
-
-
-# Build packages
-#make package/feeds/packages/luasocket/download
-#make package/feeds/packages/luasocket/prepare
-#make package/feeds/packages/luasocket/compile
-#make package/feeds/packages/luasocket/install
-#make package/index
 make V=s
